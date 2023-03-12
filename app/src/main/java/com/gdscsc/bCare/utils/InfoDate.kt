361e112dfc4392bf8d64f302ae82759fe93d1b3b
moisedk <moisedete@gmail.com>
2023-03-11 20:35:56 -0500

    override equals() and hashCode()

--- app/src/main/java/com/gdscsc/bCare/utils/InfoDate.kt
+++ app/src/main/java/com/gdscsc/bCare/utils/InfoDate.kt
@@ -6,15 +6,15 @@ import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import java.time.temporal.Temporal
 import java.util.*
-
-class InfoDate (startTime: LocalDateTime) {
+/*Date formatting utility class for CareInfo. The startDate can either come from recording a sleep, nursing, or diaper change event*/
+class InfoDate (startTime: LocalDateTime?) {
     var dayName: String
     var dayNum: String
     var month: String
     var year: String
 
     init {
-        val date = Date.from(startTime.toInstant(ZoneOffset.UTC ))
+        val date = Date.from(startTime?.toInstant(ZoneOffset.UTC ))
         val dateFormat = SimpleDateFormat("MM-dd-E-", Locale.getDefault()) // Needs to test with different locales
         val dateString: String = dateFormat.format(date)
         val dateParts = dateString.split("-")
@@ -22,5 +22,17 @@ class InfoDate (startTime: LocalDateTime) {
         dayNum = dateParts[1]
         month = dateParts[0]
         year = dateParts[3]
+    }
+
+    override fun equals(other: Any?): Boolean = other is InfoDate
+            && other.dayNum == this.dayNum
+            && other.month == this.month
+            && other.year == this.year
+
+    override fun hashCode(): Int {
+        var result = dayNum.hashCode()
+        result = 31 * result + month.hashCode()
+        result = 31 * result + year.hashCode()
+        return result
     }
 }
